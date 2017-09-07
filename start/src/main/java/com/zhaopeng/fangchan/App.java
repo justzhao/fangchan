be6a1f03fc634bc4a4ea3993035bf6e699c74565
zhaopeng <zp6282154@163.com>
2017-09-08 00:23:31 +0800

    feature: 引入spring

--- start/src/main/java/com/zhaopeng/fangchan/App.java
+++ start/src/main/java/com/zhaopeng/fangchan/App.java
@@ -1,13 +1,25 @@
 package com.zhaopeng.fangchan;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.concurrent.CountDownLatch;
+
+@EnableAutoConfiguration(exclude = {DataSourceTransactionManagerAutoConfiguration.class,
+        SecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.zhaopeng.fangchan"})
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().sources(App.class).web(false).addCommandLineProperties(true).run();
+        //阻塞主线程
+        try {
+            new CountDownLatch(1).await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ start/src/main/java/com/zhaopeng/fangchan/config/Configurater.java
@@ -0,0 +1,14 @@
+package com.zhaopeng.fangchan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by zhaopeng on 2017/9/8.
+ */
+
+
+@Configuration
+@ImportResource({"classpath:common/application_base.xml"})
+public class Configurater {
+}
